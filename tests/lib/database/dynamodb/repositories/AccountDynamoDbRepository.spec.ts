deae6fbba9adcbcfff00a62481c2a891ba5ba278
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-05 13:07:48 -0300

    test: ensure AccountDynamoDbRepository returns an acconunt on success

---
+++ tests/lib/database/dynamodb/repositories/AccountDynamoDbRepository.spec.ts
@@ -0,0 +1,30 @@
+import { AccountDynamoDbRepository } from '../../../../../lib/database/dynamodb/repositories/AccountDynamoDbRepository'
+import { DynamoDbHelper } from '../../../../../lib/database/dynamodb/helpers/DynamoDbHelper'
+
+describe('Account DynamoDb Repository', () => {
+  beforeEach(() => {
+    DynamoDbHelper.connect({
+      endpoint: process.env.MOCK_DYNAMODB_ENDPOINT,
+      sslEnabled: false,
+      region: 'local'
+    })
+  })
+
+  test('Should return an account on succes', async () => {
+    const sut = new AccountDynamoDbRepository()
+    const account = await sut.add({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any@mail.com',
+      password: 'any_password'
+    })
+  })
+})
