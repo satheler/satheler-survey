a73c98b2ec35c57b8a9ee97d37e4ebae5f9cf1cb
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-06 13:25:28 -0300

    feat: ensure AccountAuthenticationController calls EmailValidator with correct email

--- app/controllers/authentication/AccountAuthenticationController.ts
+++ app/controllers/authentication/AccountAuthenticationController.ts
@@ -1,7 +1,11 @@
-import { Controller, ControllerContext, HttpResponse } from '../../../contracts'
+import { Controller, ControllerContext, EmailValidator, HttpResponse } from '../../../contracts'
 import { MissingParamError } from '../../errors'
 
 export class AccountAuthenticationController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) { }
+
   async handle ({ request, response }: ControllerContext): Promise<HttpResponse> {
     if (!request.body.email) {
       return response.badRequest(new MissingParamError('email'))
@@ -10,5 +14,7 @@ export class AccountAuthenticationController implements Controller {
     if (!request.body.password) {
       return response.badRequest(new MissingParamError('password'))
     }
+
+    this.emailValidator.isValid(request.body.email)
   }
 }
