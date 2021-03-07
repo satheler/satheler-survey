c01e2807f8c1ae0f0dc918800688cd20cc1f258c
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-07 14:06:19 -0300

    test: ensure RequiredFieldValidation returns nothing if validation succeeds

--- tests/lib/validation/RequiredFieldValidation.spec.ts
+++ tests/lib/validation/RequiredFieldValidation.spec.ts
@@ -4,8 +4,15 @@ import { RequiredFieldValidation } from '../../../lib/validation/RequiredFieldVa
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('any_field')
-    const error = sut.validate({ another_field: '' })
+    const error = sut.validate({ another_field: 'another_field_value' })
 
     expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  test('Should return nothing if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({ any_field: 'any_field_value' })
+
+    expect(error).toBeFalsy()
   })
 })
