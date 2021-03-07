bb0217c804d7f4dd5fc5dbcc8e3e6530e94df167
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-07 17:08:00 -0300

    feat: ensure DatabaseAccountAuthentication calls TokenGenerator with correct id

--- app/usecases/authentication/DatabaseAccountAuthentication.ts
+++ app/usecases/authentication/DatabaseAccountAuthentication.ts
@@ -1,16 +1,17 @@
 import { HashComparer } from '../../../contracts'
+import { TokenGenerator } from '../../../contracts/token-generator'
 import { AccountAuthentication, AccountAuthenticationParams } from '../../domain/entities/Account'
 import { FindAccountByEmailRepository } from '../../repositories/contracts/account/FindAccountByEmailRepository'
 
 export class DatabaseAccountAuthentication implements AccountAuthentication {
   constructor (
     private readonly findAccountByEmailRepository: FindAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) { }
 
   async auth ({ email, password }: AccountAuthenticationParams): Promise<string> {
     const account = await this.findAccountByEmailRepository.find({ email })
-
     if (!account) {
       return null
     }
@@ -19,5 +20,7 @@ export class DatabaseAccountAuthentication implements AccountAuthentication {
     if (!isValidPassword) {
       return null
     }
+
+    await this.tokenGenerator.generate(account.id)
   }
 }
