9a58489a204fc75237896f47d71e85a09d7807ee
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-14 20:28:13 -0300

    feat: add account and api token migrations

---
+++ database/migrations/1592489784670_api_tokens.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class ApiTokens extends BaseSchema {
+  protected tableName = 'api_tokens'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('account_id', 36).references('id').inTable('accounts').onDelete('CASCADE')
+      table.string('name').notNullable()
+      table.string('type').notNullable()
+      table.string('token', 64).notNullable()
+
+      /**
+       * "useTz: true" utilizes timezone option in PostgreSQL and MSSQL
+       */
+      table.timestamp('expires_at', { useTz: true }).nullable()
+      table.timestamp('created_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
