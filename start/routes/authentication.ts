3c02c2dbdf8e29796a06874b8c6504efa12af417
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-14 20:34:20 -0300

    feat: register LogoutController in authentication routes

--- start/routes/authentication.ts
+++ start/routes/authentication.ts
@@ -1,3 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/', 'Authentication/Account/AccountAuthenticationController')
+Route.any('/logout', 'Authentication/Logout/LogoutController')
