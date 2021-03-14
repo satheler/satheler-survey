9a29a22d299dfdbd681e0554b5dda19ff5be60a8
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-14 20:35:25 -0300

    refactor: rename LogoutController to LogoutAccountController

--- app/Controllers/Http/Authentication/LogoutAccount/LogoutAccountController.ts
+++ app/Controllers/Http/Authentication/LogoutAccount/LogoutAccountController.ts
@@ -1,6 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class LogoutController {
+export default class LogoutAccountController {
   public async handle ({ auth, response }: HttpContextContract) {
     await auth.logout()
     return response.status(200)
--- start/routes/authentication.ts
+++ start/routes/authentication.ts
@@ -1,4 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/', 'Authentication/Account/AccountAuthenticationController')
-Route.any('/logout', 'Authentication/Logout/LogoutController')
+Route.any('/logout', 'Authentication/LogoutAccount/LogoutAccountController')
