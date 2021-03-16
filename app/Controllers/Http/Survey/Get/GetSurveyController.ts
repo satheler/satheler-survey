6ae834c988e2b2ca8588695522c649ddaa7b35cc
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-15 22:17:11 -0300

    feat: get questions together on survey get

--- app/Controllers/Http/Survey/Get/GetSurveyController.ts
+++ app/Controllers/Http/Survey/Get/GetSurveyController.ts
@@ -4,6 +4,7 @@ import { Survey } from 'App/Models'
 export default class GetSurveyController {
   public async handle ({ params, response }: HttpContextContract) {
     const survey = await Survey.findOrFail(params.id)
+    await survey.preload('questions')
     return response.ok(survey)
   }
 }
