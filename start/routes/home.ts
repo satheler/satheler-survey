26f1cefdd179b6f510fa3ed3d38e83e4be0e777b
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-04-07 01:50:18 -0300

    feat: add cloud provider on home route

--- start/routes/home.ts
+++ start/routes/home.ts
@@ -1,7 +1,9 @@
+import Env from '@ioc:Adonis/Core/Env'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ response }) => {
   return response.status(200).json({
     app: 'satheler-survey',
+    cloud_provider: Env.get('DB_CONNECTION', 'local'),
   })
 })
