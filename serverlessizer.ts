040c99f1817ff8675dfc4523f67a7946e487258b
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-04-10 19:13:44 -0300

    feat: run migrations on serverlessize runs on providers

--- serverlessizer.ts
+++ serverlessizer.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata'
+import execa from 'execa'
 import { ServerContract } from '@ioc:Adonis/Core/Server'
 import { Ignitor } from '@adonisjs/core/build/standalone'
 import Serverlessize from '@satheler/s12r'
@@ -20,9 +21,14 @@ async function bootstrapServer () {
   return server
 }
 
+function runMigration () {
+  execa.commandSync('node ace migration:run --force')
+}
+
 export const handle = async (...args: any[]) => {
   if(!server) {
     server = await bootstrapServer()
+    runMigration()
   }
 
   const { request, response } = Serverlessize(args)
