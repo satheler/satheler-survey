bad3efbe4c3ff3c1143c01f42d9a111b0861161a
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-04-30 17:43:39 -0300

    fix: increase idle timeout on database connections to 1 millis

--- config/database.ts
+++ config/database.ts
@@ -76,7 +76,7 @@ function makeConnections () {
           database: Env.get(`DB_${key}NAME`),
         },
         pool: {
-          idleTimeoutMillis: 0,
+          idleTimeoutMillis: 1,
         },
         healthCheck: alternativeDatabaseNameLowercase === Env.get('DB_CONNECTION', 'main'),
         debug: true,
